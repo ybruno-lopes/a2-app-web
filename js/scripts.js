11d1791f8ccce1d20debec2362410362334c9cef
bruno lopes <b_lopes98@hotmail.com>
2023-11-30 19:19:33 -0300

    a2pronta

---
+++ js/scripts.js
@@ -0,0 +1,35 @@
+const _gallery = [
+  {
+    img: "assets/photo_0.png",
+    description: "Conceição do Mato Dentro",
+  },
+  {
+    img: "assets/photo_1.png",
+    description: "Diamantina",
+  },
+  {
+    img: "assets/photo_2.png",
+    description: "Tiradentes",
+  },
+  {
+    img: "assets/photo_3.png",
+    description: "São Lourenço",
+  },
+  {
+    img: "assets/photo_4.png",
+    description: "Serro",
+  },
+  {
+    img: "assets/photo_5.png",
+    description: "São Tomé das Letras",
+  },
+  {
+    img: "assets/photo_6.png",
+    description: "Ipoema",
+  },
+  {
+    img: "assets/photo_7.png",
+    description: "Ouro Preto",
+  },
+];
+
